0ea7f509c125d866bdf956e746a624973bb9c514
AaravYadav99 <kumaryadavanjani@gmail.com>
2024-09-15 20:14:42 +0545

    fanily project

---
+++ html/slides.js
@@ -0,0 +1,14 @@
+var index=0;
+function carousel(){
+    var i;
+    var x=document.getElementsByClassName("myslides");
+    for (i=0; < x.length  i++)
+        {i}style.display="none"
+}
+index++;
+if(index>x.lngth)
+  {  index-1;
+x[index-1].styledisplay="blcok";
+setTimeout(carousel,2000)
+}
+carousel();
